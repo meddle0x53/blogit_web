5950f51179c2df8a15a80f57d93171e84019aaac
meddle <meddlesmerch@gmail.com>
2017-09-30 18:20:13 +0300

    Add lang=<language> multicode logic.

--- web/static/js/multicode.js
+++ web/static/js/multicode.js
@@ -90,7 +90,10 @@ var MultyCode = {
 
     $('em:contains(multi-code)').each((index, el) => {
       let $codes = $(el).prevUntil(':not(code)');
-      $codes.length = allLanguages.length;
+
+      if ($codes.length > allLanguages.length) {
+        $codes.length = allLanguages.length;
+      }
       $codes.addClass('multi-inline');
 
       $codes.each((index, el) => {

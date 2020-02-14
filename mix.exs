ad851df7355863f8597af9005bd183cfcc626922
9to1url <9to1url@gmail.com>
2020-02-14 16:49:40 -0500

    upgrade to phx 1.4, but no touch folder structure and brunch

--- mix.exs
+++ mix.exs
@@ -16,7 +16,7 @@ defmodule BlogitWeb.Mixfile do
   def application do
     [mod: {BlogitWeb, []},
      applications: [:blogit, :calendar, :phoenix, :phoenix_pubsub,
-      :phoenix_html, :cowboy, :logger, :gettext, :edeliver],
+      :phoenix_html, :plug_cowboy,:plug, :poison, :logger, :gettext, :edeliver],
     included_applications: [
       :calendar_translations, :earmark, :git_cli, :logger_file_backend
     ]]
@@ -27,12 +27,15 @@ defmodule BlogitWeb.Mixfile do
   defp elixirc_paths(_),     do: ["lib"]
 
   defp deps do
-    [{:phoenix, "~> 1.3"},
+    [{:phoenix, "~> 1.4"},
      {:phoenix_pubsub, "~> 1.0"},
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"},
+     {:poison, "~> 3.1.0"},
+     {:jason, "~> 1.0"},
+     {:plug_cowboy, "~> 2.0"},
+     {:plug, "~> 1.7"},
      {:calendar, "~> 0.17"},
      {:calendar_translations, "~> 0.0.4"},
      {:edeliver, "~> 1.4"},
